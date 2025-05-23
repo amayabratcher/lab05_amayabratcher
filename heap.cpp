646e521b8749a85ef1070d024b942e6b9f51bcf1
Amaya <amayabratcher@ucsb.edu>
2025-05-23 01:10:57 -0700

    implemented pop

--- heap.cpp
+++ heap.cpp
@@ -3,18 +3,18 @@
 
 #include "heap.h"
 #include <iostream>
-using std::cout;
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 // Pushes a value into the heap, then ensures
 // the heap is correctly arranged
 void Heap::push(int value){
 
-vdata.push_back(value);
-int idx = (vdata.size())-1;
-while(idx > 0 && vdata.at((idx-1)/2) > vdata.at(idx)){
-  int parentdata = vdata.at((idx-1)/2);
-  vdata.at(idx) = parentdata;
-  vdata.at((idx-1)/2) = value;
+  vdata.push_back(value);
+  int idx = (vdata.size())-1;
+  while(idx > 0 && vdata.at((idx-1)/2) > vdata.at(idx)){
+  swap(vdata.at(idx), vdata.at((idx-1)/2));
   idx = ((idx-1)/2);
 }
 
@@ -25,18 +25,40 @@ while(idx > 0 && vdata.at((idx-1)/2) > vdata.at(idx)){
 // (but does not return it), then ensures
 // the heap is correctly arranged
 void Heap::pop(){
-int idx = ((vdata.size())-1);
-vdata.at(0) = vdata.at(idx);
-vdata.pop_back();
-idx--;
-int curridx = 0;
-while(curridx <= idx && (vdata.at(curridx) >(vdata.at(curridx*2+1)|| vdata.at(curridx*2+2))))
-
+  if(!(this -> empty())){
+    vdata[0] = vdata[vdata.size()-1];
+    vdata.pop_back();
+    int idx = 0;
+    int smaller = 0;
+    while(idx < vdata.size()){
+      if(idx*2+2 >= vdata.size() && idx*2+1 >= vdata.size()){
+          break;
+      }
+      else if(idx*2+2 >= vdata.size()){
+        smaller = idx*2+1;
+        if( vdata.at(idx) > vdata.at(idx*2+1)){
+        swap(vdata.at(idx), vdata.at(idx*2+1));
+        idx = idx*2+1;
+        }
+      }
+      else{
+        smaller = vdata.at(idx*2+1) > vdata.at(idx*2+2)? (idx*2+2) : (idx*2+1);
+        if(vdata.at(idx) > vdata.at(smaller)){
+        swap(vdata.at(idx), vdata.at(smaller));
+          idx = smaller;
+      }
+      }
+    }
+  }
 
 }
 
 // Returns the minimum element in the heap
 int Heap::top(){
+  if (vdata.size() == 0){
+    return -1;
+  }
+  
   return vdata[0];
 }
 
